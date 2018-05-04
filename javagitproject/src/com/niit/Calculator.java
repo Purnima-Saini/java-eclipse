acc5bb7b6c65d45efdf11cf6b3d6f65d2b2dc5b6
Training <purnimasaini2704@gmail.com>
2018-05-04 12:12:55 +0530

    Calculator V 1.0

---
+++ javagitproject/src/com/niit/Calculator.java
@@ -0,0 +1,23 @@
+package com.niit;
+
+public class Calculator 
+{
+	Integer a,b;
+	Calculator(Integer a,Integer b)
+	{
+		this.a=a;
+		this.b=b;
+	}
+	public int getA() {
+		return a;
+	}
+	public void setA(Integer a) {
+		this.a = a;
+	}
+	public int getB() {
+		return b;
+	}
+	public void setB(Integer b) {
+		this.b = b;
+	}
+}
